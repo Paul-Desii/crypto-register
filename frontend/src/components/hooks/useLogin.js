f768773aac3b4357d9578e6848eafd1d7be8ebd1
Paul-Desii <pauldeskidial@gmail.com>
2022-10-28 01:24:24 +0100

    routerFix

--- frontend/src/components/hooks/useLogin.js
+++ frontend/src/components/hooks/useLogin.js
@@ -13,7 +13,7 @@ export const useLogin = () => {
         setIsloading(true)
         setError(null)
 
-        const response = await fetch('/api/user/login', {
+        const response = await fetch('http://localhost:10000/api/user/login', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({email, password})
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,18 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CryptoContextProvider } from './components/context/CryptoContext'
 import { AuthContextProvider } from './components/context/AuthContext';
-import { HashRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    
     <AuthContextProvider>
       <CryptoContextProvider>
         <App />
       </CryptoContextProvider>
     </AuthContextProvider>
-    </HashRouter>
+    
   </React.StrictMode>
 );
 
